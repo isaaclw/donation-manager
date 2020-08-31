9efd750a4c0ec0bd18e0137dbe71cb5410ebfaa1
isaaclw <isaaclw@gmail.com>
2020-08-31 10:31:19 -0400

    Create model structure, minus authentication

--- app/donations/models.py
+++ app/donations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class DonationCategory(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+
+class RequestedDonations(models.Model):
+    name = models.CharField()
+    amount_needed = models.IntegerField()
+    category = models.ForeignKey(DonationCategory, on_delete=models.CASCADE)
+
+class Donor(models.Model):
+    address = models.CharField()
+    phone_number = models.CharField()
+    email_address = models.CharField()
+    contact_name = models.CharField()
+    organization = models.CharField()
+    # key based authfor update?
+
+class DonationOffer(models.Model):
+    amount = models.IntegerField()
+    donation = models.ForeignKey(RequestedDonation, on_delete=models.CASCADE)
+
